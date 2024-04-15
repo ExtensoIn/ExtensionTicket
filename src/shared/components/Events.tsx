5d2ef6c2f0ef1107f2a6442abf2b166571e684e5
Joel <rodrigoquiroz308@gmail.com>
2024-04-15 13:53:30 -0400

    fix: responsive grid

--- src/shared/components/Events.tsx
+++ src/shared/components/Events.tsx
@@ -38,10 +38,7 @@ const Events = () => {
     return (
         <section className='flex flex-col items-center justify-center  bg-gray-100 p-20 md:p-40'>
             <h1 className='text-4xl font-light font-AbrilFatface text-left w-full text-blue-950'>Upcoming Events</h1>
-            {/* responsive grid */}
-            <div className='
-
-            grid  md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 mt-20 w-full'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 mt-20 w-full'>
                 {
                     events.map((event, index) => {
                         return (
