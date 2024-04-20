c0abdf7a0fa2a970c6d4238aca08c1feccb6be38
Joel <rodrigoquiroz308@gmail.com>
2024-04-20 00:20:23 -0400

    test login

--- src/routes/login/index.lazy.tsx
+++ src/routes/login/index.lazy.tsx
@@ -1,5 +1,14 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { login } from '../../connection/user'
 
 export const Route = createLazyFileRoute('/login/')({
-  component: () => <div>Hello /login/!</div>
+  component: () => <>
+    <button 
+    onClick={() => {
+      login('email', 'password')
+    }}
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute mt-80">
+      Button
+    </button>
+  </>
 })
