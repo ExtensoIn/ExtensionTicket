265a72e977be72ea268baac05d65bec23910b079
Joel <rodrigoquiroz308@gmail.com>
2024-04-20 01:03:01 -0400

    add Not found page

---
+++ src/shared/components/NotFound.tsx
@@ -0,0 +1,17 @@
+import bg from '../../assets/backgroundHome.webp'
+import ImageLinear from './ImageLinear';
+
+const NotFound = () => {
+    return (
+        <ImageLinear height='100vh'>
+            <div className='flex flex-col h-full justify-center items-center'>
+                <h1 className='text-white text-4xl font-AbrilFatface font-light'>
+                    No encontramos la página que buscas :(
+
+                </h1>
+            </div>
+        </ImageLinear>
+    )
+}
+
+export default NotFound;
