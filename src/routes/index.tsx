c5da09ebf03706ef6323f694a4293a2c4d781292
LaBalaElmo <jrttaquichiri@gmail.com>
2024-04-27 23:19:36 -0400

    Corrections 11pm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,9 +14,9 @@ import medium from '../assets/home/brands/medium.png'
 import stripe from '../assets/home/brands/stripe.png'
 import uber from '../assets/home/brands/uber.png'
 import PageBanner from '../shared/components/layout/PageBanner'
-import { Link, createFileRoute, useNavigate } from '@tanstack/react-router'
-import { useQuery } from '@tanstack/react-query'
-import { getEvents } from '../connection/event'
+import {createFileRoute, useNavigate} from '@tanstack/react-router'
+import {useQuery} from '@tanstack/react-query'
+import {getEvents} from '../connection/event'
 
 export const Route = createFileRoute('/')({
     component: Home,
@@ -33,15 +33,15 @@ function Home() {
     return (
         <div className='flex flex-col'>
             <ImageLinear height='100vh'>
-                <HomeBanner />
+                <HomeBanner/>
             </ImageLinear>
             <Events
                 title='Upcoming Events'
                 events={eventsQuery.data || []}
                 showImage={true}
             />
-            <CreateEvent />
-            <Brands />
+            <CreateEvent/>
+            <Brands/>
         </div>
     )
 }
@@ -51,7 +51,7 @@ function HomeBanner() {
     const homeInfo = {
         imgSrc: petIcon,
         title: 'Unlock the blockchain power, one ticket at a time',
-        description: "Secure your spot now and join the digital revolution! Tickets selling fast – don\'t miss out!",
+        description: "Secure your spot now and join the digital revolution! Tickets selling fast – don't miss out!",
     }
     return (
         <div className='flex flex-col h-full w-full justify-center items-center'>
@@ -70,8 +70,9 @@ function HomeBanner() {
 function CreateEvent() {
     const navigate = useNavigate();
     return (
-        <section className='font-DMSans bg-purple-200 w-full flex flex-col gap-4 md:gap-10 justify-center items-center px-6 pb-6 md:max-h-64 md:flex-row'>
-            <img className='relative md:bottom-20' src={dogIcon} alt="Dog banner icon" />
+        <section
+            className='font-DMSans bg-purple-200 w-full flex flex-col gap-4 md:gap-10 justify-center items-center px-6 pb-6 md:max-h-64 md:flex-row'>
+            <img className='relative md:bottom-20' src={dogIcon} alt="Dog banner icon"/>
             <span className='flex flex-col gap-2 text-center md:text-left'>
                 <h3 className='text-3xl font-bold '>Make your own event</h3>
                 <p>Start creating your own event and sell tickets</p>
@@ -106,7 +107,7 @@ function Brands() {
             <span className='grid grid-cols-2 md:grid-cols-3 
             lg:grid-cols-5 items-center content-center justify-items-center auto-rows-fr gap-2'>
                 {images.map((image, index) => (
-                    <img className='w-full xs:w-auto' key={index} src={image} alt="Brand" />
+                    <img className='w-full xs:w-auto' key={index} src={image} alt="Brand"/>
                 ))}
             </span>
         </section>
